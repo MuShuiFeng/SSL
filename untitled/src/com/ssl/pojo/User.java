fd2fd2fc665a69246ca26cd24f0ba04282da1fff
123 <123>
2020-08-20 09:42:01 +0800

    123

---
+++ untitled/src/com/ssl/pojo/User.java
@@ -0,0 +1,10 @@
+package com.ssl.pojo;
+
+public class User {
+    public void add(){
+        System.out.println("add");
+    }
+    public void delete(){
+        System.out.println("delete");
+    }
+}
